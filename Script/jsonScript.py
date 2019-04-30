b341b18d0276893b76bd82f558466d044ba55a99
Riley-Kilgore <riley_kilgore@outlook.com>
2019-04-30 03:00:10 -0700

    Added a file for .json generation. No more html!

---
+++ Script/jsonScript.py
@@ -0,0 +1,18 @@
+# The following script is to generate the json files
+#   representing a plan with preferences and rating.
+import os
+
+# Preference Elements
+
+
+def getPreferences():
+  return null
+
+def getQuarters():
+  for (q in quarters):
+    
+def printSchedule():
+
+
+class Quarter:
+  
