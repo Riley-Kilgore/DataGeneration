8bd717f0d9862e13f6c16adf7d1cc71be221b602
Riley-Kilgore <riley_kilgore@outlook.com>
2019-04-30 17:28:01 -0700

    Collaborated on the stuff.

--- Script/jsonScript.py
+++ Script/jsonScript.py
@@ -1,18 +1,41 @@
 # The following script is to generate the json files
 #   representing a plan with preferences and rating.
 import os
+import string
+import json
 
-# Preference Elements
+#JSON generative code.
+def write_file(filename, data):
+    with open(filename + '.json', 'a+') as file:
+        json.dump(data, file)
 
+class Schedule:
+    # Preferences
+    string planName
+    string major
+    string school
+    string timePref
+    string summerPref
+    string startQuarter
+    string startMath
+    string startEnglish
+    string enrollType
+    string jobType
 
-def getPreferences():
-  return null
+    #Rank
+    string scheduleGrade
+    string gradeReason
 
-def getQuarters():
-  for (q in quarters):
-    
-def printSchedule():
-
+    # Classes
+    Quarter[] classes
 
 class Quarter:
-  
+    string id
+    int year
+    string title
+    Course[] courses
+
+class Course:
+    string id
+    string title
+    string description
